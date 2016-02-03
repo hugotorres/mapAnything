b99c911944d8225c8b05288c5f66721dfebfc0c3
hugardo <ingenierohugotorres@gmail.com>
2016-02-03 09:31:15 -0500

    initial

---
+++ www/js/services/locationsService.js
@@ -0,0 +1,21 @@
+angular.module('starter').factory('LocationsService', [ function() {
+
+  var locationsObj = {};
+
+  locationsObj.savedLocations = [
+    {
+      name : "Bogotá D.C., Colombia",
+      description:"Capital city of Colombia",
+      cateogry:"City",
+      lat : 4.624335,
+      lng : -74.063644
+    },
+  //TODO
+  //falta implementar la funcionalidad para guardar en localstorage y luego cuando detecte conexion a internet subirlo a firebase o parse.
+  //
+
+  ];
+
+  return locationsObj;
+
+}]);
